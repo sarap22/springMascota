50b9e62a2ce7dc67a2d8b2c1f84bf624edfe5b18
Sara <saritaprieto445@gmail.com>
2023-03-08 11:13:57 -0500

    creacion proyecto springMascota

---
+++ src/main/java/com/Mascota/demo/SpringMascotaApplication.java
@@ -0,0 +1,13 @@
+package com.Mascota.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMascotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMascotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mascota/demo/SpringMascotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mascota.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMascotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
